eb7c3e362a465ba7447cb16f4d5dd0341aa5a25e
wiater88 <wiater88@gmail.com>
2022-07-06 15:27:36 +0100

    log4j with config added

--- SortManagerApp/src/main/java/com/sparta/Main.java
+++ SortManagerApp/src/main/java/com/sparta/Main.java
@@ -1,7 +1,12 @@
 package com.sparta;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    public static Logger logger = LogManager.getLogger("My Logger"); // load property file
     public static void main(String[] args) {
+        logger.info("First log");
      Loader loader = new Loader();
      loader.run();
 
